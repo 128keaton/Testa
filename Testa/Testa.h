26e0cdb621007046d76afb1cd5aed1eb1d8c2eea
Keaton Burleson <keaton.burleson@me.com>
2017-12-11 13:46:52 -0600

    Initial Commit

---
+++ Testa/Testa.h
@@ -0,0 +1,19 @@
+//
+//  Testa.h
+//  Testa
+//
+//  Created by Keaton Burleson on 12/11/17.
+//  Copyright © 2017 Keaton Burleson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Testa.
+FOUNDATION_EXPORT double TestaVersionNumber;
+
+//! Project version string for Testa.
+FOUNDATION_EXPORT const unsigned char TestaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Testa/PublicHeader.h>
+
+
